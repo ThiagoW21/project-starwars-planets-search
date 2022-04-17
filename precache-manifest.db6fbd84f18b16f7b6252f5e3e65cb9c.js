be3fd70401210658bec8c50d01e31e8467cd2a21
ThiagoW21 <ThiagoW21@users.noreply.github.com>
2022-04-17 18:24:02 +0000

    deploy: 602f90cbcd989c4b5fd3f7b08ef732e0f3a58334

---
+++ precache-manifest.db6fbd84f18b16f7b6252f5e3e65cb9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be06f55b6784bf29651df1727a1dd4f",
+    "url": "/project-starwars-planets-search/index.html"
+  },
+  {
+    "revision": "56e150829158d9a0a9e2",
+    "url": "/project-starwars-planets-search/static/css/main.bdf6c3e4.chunk.css"
+  },
+  {
+    "revision": "fbd8e6073c1f36bb0155",
+    "url": "/project-starwars-planets-search/static/js/2.bda8ee4d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/project-starwars-planets-search/static/js/2.bda8ee4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e150829158d9a0a9e2",
+    "url": "/project-starwars-planets-search/static/js/main.d6c5d11a.chunk.js"
+  },
+  {
+    "revision": "f7eb459dd634f26d2408",
+    "url": "/project-starwars-planets-search/static/js/runtime-main.af1aea79.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github-page/precache-manifest.2634b64d6b6bafa9385e1ec9db249156.js"
+  "/project-starwars-planets-search/precache-manifest.db6fbd84f18b16f7b6252f5e3e65cb9c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-page/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-starwars-planets-search/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
